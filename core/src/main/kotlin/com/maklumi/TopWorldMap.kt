ae7e13dcd7fd4eb0ca4a6360a920ab8a15059e78
Maklumi <maklumi@yahoo.com>
2020-01-19 17:00:16 +0800

    Ch08 1) Added song for Top World (including updating copyright notice)

--- core/src/main/kotlin/com/maklumi/TopWorldMap.kt
+++ core/src/main/kotlin/com/maklumi/TopWorldMap.kt
@@ -1,6 +1,8 @@
 package com.maklumi
 
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.maklumi.audio.AudioObserver.AudioCommand.*
+import com.maklumi.audio.AudioObserver.AudioTypeEvent.MUSIC_TOPWORLD
 
 class TopWorldMap : Map(MapFactory.MapType.TOP_WORLD, "maps/topworld.tmx") {
 
@@ -10,8 +12,11 @@ class TopWorldMap : Map(MapFactory.MapType.TOP_WORLD, "maps/topworld.tmx") {
     }
 
     override fun playMusic() {
+        notify(MUSIC_LOAD, MUSIC_TOPWORLD)
+        notify(MUSIC_PLAY_LOOP, MUSIC_TOPWORLD)
     }
 
     override fun stopMusic() {
+        notify(MUSIC_STOP, MUSIC_TOPWORLD)
     }
 }
