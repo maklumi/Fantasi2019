d7d9f1c472533cd8309ca758cfe95600b1adf0ff
Maklumi <maklumi@yahoo.com>
2020-01-20 20:11:32 +0800

    Ch08 1) Added song for Castle of Doom

--- core/src/main/kotlin/com/maklumi/CastleDoomMap.kt
+++ core/src/main/kotlin/com/maklumi/CastleDoomMap.kt
@@ -1,6 +1,8 @@
 package com.maklumi
 
 import com.badlogic.gdx.graphics.g2d.Batch
+import com.maklumi.audio.AudioObserver.AudioCommand
+import com.maklumi.audio.AudioObserver.AudioTypeEvent
 
 class CastleDoomMap : Map(MapFactory.MapType.CASTLE_OF_DOOM, "maps/castle_of_doom.tmx") {
 
@@ -10,8 +12,11 @@ class CastleDoomMap : Map(MapFactory.MapType.CASTLE_OF_DOOM, "maps/castle_of_doo
     }
 
     override fun playMusic() {
+        notify(AudioCommand.MUSIC_LOAD, AudioTypeEvent.MUSIC_CASTLEDOOM)
+        notify(AudioCommand.MUSIC_PLAY_LOOP, AudioTypeEvent.MUSIC_CASTLEDOOM)
     }
 
     override fun stopMusic() {
+        notify(AudioCommand.MUSIC_STOP, AudioTypeEvent.MUSIC_CASTLEDOOM)
     }
 }
